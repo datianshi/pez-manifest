2ad9de870f1daa16fc7c51c9e2e69aebe3f3d162
datianshi <dsz0111@gmail.com>
2016-02-25 09:46:27 -0600

    Roll back to logsearch v7.5

--- elk/elk.sh
+++ elk/elk.sh
@@ -1,5 +1,6 @@
-spruce merge \
+spiff merge \
   templates/logsearch-deployment.yml \
   templates/logsearch-jobs.yml \
+  templates/logsearch-filters.yml \
   logsearch-infrastructure-vcloud.yml \
   stub.yml
